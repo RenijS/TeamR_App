5259fb2a51fc64eaeb96003e1bd66c6d0a5e9961
RenijS <renijshrestha8@gmail.com>
2020-10-03 23:00:40 +1000

    Added settings for profile photo, added toolbar with menu containing logout and delete account.

--- app/src/main/java/com/example/assign/StartActivity.java
+++ app/src/main/java/com/example/assign/StartActivity.java
@@ -1,11 +1,11 @@
 package com.example.assign;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
